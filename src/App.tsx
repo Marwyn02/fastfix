3c4d39bc0122eee5d3ee86ffc3ecaff763e39f09
Marwyn <jhunmarwyn.sumargo@gmail.com>
2026-06-03 11:02:21 +0800

    fix analytics error

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import PageNotFound from '@/lib/PageNotFound';
 import ScrollToTop from '@/components/ScrollToTop';
 import Layout from '@/components/Layout';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const AuthenticatedApp = () => {
   return (
